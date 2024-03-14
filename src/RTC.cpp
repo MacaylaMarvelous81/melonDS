9a85f24a2a0868ccb3594a2380aecd2cc864a8a8
Jomar Milan <jomarm@jomarm.tech>
2024-03-13 20:34:36 -0700

    Restore RTC functionality
    Removes the RTC hack so that the real-time clock ticks once again.

--- src/RTC.cpp
+++ src/RTC.cpp
@@ -133,7 +133,7 @@ void ByteIn(u8 val)
 
             case 0x20:
                 {
-                    /*time_t timestamp = time(NULL);
+                    time_t timestamp = time(NULL);
                     struct tm timedata;
                     localtime_r(&timestamp, &timedata);
 
@@ -143,14 +143,7 @@ void ByteIn(u8 val)
                     Output[3] = BCD(timedata.tm_wday);
                     Output[4] = BCD(timedata.tm_hour);
                     Output[5] = BCD(timedata.tm_min);
-                    Output[6] = BCD(timedata.tm_sec);*/
-                    Output[0] = BCD(22);
-                    Output[1] = BCD(11);
-                    Output[2] = BCD(3);
-                    Output[3] = BCD(3);
-                    Output[4] = BCD(6);
-                    Output[5] = BCD(0);
-                    Output[6] = BCD(0);
+                    Output[6] = BCD(timedata.tm_sec);
                 }
                 break;
 
